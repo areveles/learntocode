d4c8711109f23d0adefb72be535a6d157f9550a2
areveles <areveles@student.umgc.edu>
2024-07-22 20:18:01 -0700

    Exercise 7-1

--- learn-java/java-explorer/ShopApp/src/duke/choice/Clothing.java
+++ learn-java/java-explorer/ShopApp/src/duke/choice/Clothing.java
@@ -16,21 +16,20 @@ public class Clothing {
     public double getPrice() {
         return price + (price * TAX_RATE);
     }
+
+    public void setPrice(double price) {
+        this.price = (price > MIN_PRICE) ? price : MIN_PRICE;
+    }
     
     public String getDescription() {
         return description;
     }
 
-    public String getSize() {
-        return size;
-    }
-
-    public void setPrice(double price) {
-        this.price = (price > MIN_PRICE) ? price : MIN_PRICE;
-    }
-
     public void setDescription(String description) {
         this.description = description;
+    }
+    public String getSize() {
+        return size;
     }
 
     public void setSize(String size) {
--- learn-java/java-explorer/ShopApp/src/duke/choice/ShopApp.java
+++ learn-java/java-explorer/ShopApp/src/duke/choice/ShopApp.java
@@ -15,12 +15,32 @@ public class ShopApp {
 
         Clothing[] items = { item1, item2, new Clothing("Green Scarf", 5.0, "S"), new Clothing("Blue T-Shirt", 10.5, "S") };
 
+        int measurement = 8;
+
         c1.addItems(items);
 
-        System.out.println("Hello, " + c1.getName() + "!" + "," + c1.getSize() + ", " + c1.getTotalClothingCost());
+        System.out.println("Hello, " + c1.getName() + "! " + c1.getSize() + ", " + c1.getTotalClothingCost());
 
         for (Clothing item : c1.getItems()) {
-            System.out.println(item.getDescription());
+            System.out.println("Items " + item.getDescription() + ", " + item.getSize() + ", " + item.getPrice());
         }
+
+        int average = 0;
+        int count = 0;
+
+        for (Clothing item : c1.getItems()) {
+if (item.getSize().equals("L")) {
+            count++;
+            average += item.getPrice();
+            }
+        }
+        try {
+            average = average / count;
+            System.out.println("Average price : " + average + ", Count " + count);
+        } catch (ArithmeticException e) {
+            System.out.println("Don't divide by 0");
+        }
+
     }
 }
+
